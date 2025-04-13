2f872e972993de3c57d2109de6ad13caff22e63f
Risal720 <risalsubekti1507@gmail.com>
2025-04-13 22:20:30 +0700

    update terbaru

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/room', function () {
+    return view('room');
+});
+
+Route::get('/facilities', function () {
+    return view('facilities');
+});
+
+Route::get('/reviews', function () {
+    return view('reviews');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
