f59104b0d0bc9dbc81ba826b83fc18ffe33f671b
Risal720 <risalsubekti1507@gmail.com>
2025-04-15 00:14:38 +0700

    update tgl 14 april

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,4 @@
+<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+<a {{ $attributes }} class="{{ $active ? 'rounded-md px-3 py-2 text-sm font-medium bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }}" aria-current="{{ $active ? 'page' : false }}">{{ $slot }}
+    
+</a>
--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['tittle' => 'Home Page']);
 });
 
 Route::get('/room', function () {
-    return view('room');
+    return view('room', ['tittle' => 'Room Page']);
 });
 
 Route::get('/facilities', function () {
-    return view('facilities');
+    return view('facilities', ['tittle' => 'Facilities Page']);
 });
 
 Route::get('/reviews', function () {
-    return view('reviews');
+    return view('reviews', ['tittle' => 'Reviews Page']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['tittle' => 'Contact Page']);
 });
