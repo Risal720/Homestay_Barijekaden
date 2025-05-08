25bcc7b9088d91230369f292cb7f78bc761c28cd
Jepriantoaja <jepriantozai@gmail.com>
2025-05-08 21:48:51 +0700

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,9 @@
 <x-layout>
-  <x-slot:tittle>{{ $tittle }}</x-slot:tittle>
-  <h3>Ini Contact Page</h3>
+  <x-slot:title>
+      Tentang Kami
+  </x-slot:title>
+
+  <h1>Tentang Project Kami</h1>
+  <p>Ini adalah deskripsi singkat tentang project kamu. Kamu bisa menambahkan informasi lebih detail di sini.</p>
+  <p>Misalnya, visi dan misi project kamu, tim yang terlibat, atau informasi relevan lainnya.</p>
 </x-layout>
