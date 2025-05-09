8fe960580c0e8fd2e8f0de9ed236627265d2d887
denatacahayani <denatacahayani48@gmail.com>
2025-05-09 23:09:41 +0700

    menyelesaikan fasilitas

--- resources/views/facilities.blade.php
+++ resources/views/facilities.blade.php
@@ -1,4 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+
 <x-layout>
   <x-slot:tittle>{{ $tittle }}</x-slot:tittle>
-  <h3>Ini Facilities Page</h3>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+  <div class="slider-container">
+      <div class="slider-item">
+        <img src="image/fasilitasimg/kolam.png" alt="" />
+        <div class="slider-content">
+          <h2 class="slider-title">Swimming Pool</h2>
+          <p class="slider-description">“Rasakan kesegaran tiada duanya di kolam renang kami yang dirancang untuk relaksasi dan kenyamanan maksimal—tempat sempurna melepas penat setelah seharian beraktivitas.”</p>
+          <a class="slider-action" href="#">See More</a>
+        </div>
+      </div>
+      <div class="slider-item">
+        <img src="./image/meetingroom.png" alt="" />
+        <div class="slider-content">
+          <h2 class="slider-title">Meeting Room</h2>
+          <p class="slider-description">“Temukan standar baru untuk pertemuan profesional dengan ruang meeting kami yang dapat disesuaikan untuk segala kebutuhan bisnis Anda.”</p>
+          <a class="slider-action" href="#">See More</a>
+        </div>
+      </div>
+      <div class="slider-item">
+        <img src="./image/parkiran.png" alt="" />
+        <div class="slider-content">
+          <h2 class="slider-title">Area Parkir</h2>
+          <p class="slider-description">“Fasilitas parkir luas, aman, dan bebas biaya—karena kenyamanan Anda adalah prioritas kami sejak langkah pertama.”</p>
+          <a class="slider-action" href="#">See More</a>
+        </div>
+      </div>
+      <div class="slider-item">
+        <img src="./image/gazebo.png" alt="" />
+        <div class="slider-content">
+          <h2 class="slider-title">Gazebo</h2>
+          <p class="slider-description">"Santai sejenak di gazebo nyaman kami — tempat asyik buat ngopi, ngobrol, atau sekadar menikmati angin sepoi-sepoi dan suasana tenang di tengah hotel yang bikin betah."</p>
+          <a class="slider-action" href="#">See More</a>
+        </div>
+      </div>
+    </div>
+    
+  </body>
+  </html>
 </x-layout>
